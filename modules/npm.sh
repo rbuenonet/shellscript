4eac4cf199e6ea983bcc2b096cb63712f321d5b7
renato.bueno <renato.bueno>
2025-05-12 14:24:11 -0300

    npmc and npmr

--- modules/npm.sh
+++ modules/npm.sh
@@ -1,4 +1,6 @@
-alias npmg="c; npm list -g --depth=0"   #lista todos os modulos instalados globalmente
-alias start='c; npm run start'      # Executa a função start:dev do pagache.json
+alias npmg="c; npm list -g --depth=0"       # lista todos os modulos instalados globalmente
+alias start='c; npm run start'              # Executa a função start:dev do pagache.json
 alias start:dev='c; npm run start:dev'      # Executa a função start:dev do pagache.json
-alias lint='c; npm run lint'            # Executa a função do lint do pagache.json
+alias lint='c; npm run lint'                # Executa a função do lint do pagache.json
+alias npmc="c; rm -rf node_modules dist"    # Apaga pastas de projeto usados no npm
+alias npmr="c; npm outdated"                # Lista dependencias para serem atualizadas
